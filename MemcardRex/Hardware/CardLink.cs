d88f3a4504586f02d25973829c0d3fb953c65f76
Lorenzo Miori <memoryS60@gmail.com>
2021-12-29 11:04:39 +0100

    modified CardLink driver to support serial communication over TCP

--- MemcardRex/Hardware/CardLink.cs
+++ MemcardRex/Hardware/CardLink.cs
@@ -1,11 +1,13 @@
 ﻿//PS1CardLink communication class (based on MemCARDuino)
 //Shendo 2013
+//lmiori92 2021: added support for TCP communication (e.g. Serial Wifi Bridge)
 
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.IO.Ports;
 using System.Threading;
+using System.Net.Sockets;
 
 namespace PS1CardLinkCommunication
 {
@@ -16,22 +18,49 @@ namespace PS1CardLinkCommunication
 
         //PS1CLnk communication port
         SerialPort OpenedPort = null;
+        TcpClient OpenTcpClient = null;
+        NetworkStream TcpStream = null;
 
         //Contains a software version of a detected device
         string SoftwareVersion = "0.0";
 
+        // This constructor is used to setup a Serial Port communication with PS1Link
         public string StartPS1CardLink(string ComPortName)
         {
             //Define a port to open
             OpenedPort = new SerialPort(ComPortName, 38400, Parity.None, 8, StopBits.One);
             OpenedPort.ReadBufferSize = 256;
 
-            //Buffer for storing read data from the PS1CLnk
-            byte[] ReadData = null;
-
             //Try to open a selected port (in case of an error return a descriptive string)
             try { OpenedPort.Open(); }
             catch (Exception e) { return e.Message; }
+
+            // Initial handshake
+            return InitializePS1CardLink(ComPortName);
+        }
+
+        // This constructor is used to setup a Serial Port over TCP/IP with PS1Link
+        public string StartPS1CardLink(string Address, int Port)
+        {
+            // Try to setup the communication link
+            try
+            {
+                OpenTcpClient  = new TcpClient(Address, Port);
+                TcpStream = OpenTcpClient.GetStream();
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+
+            // Initial handshake
+            return InitializePS1CardLink(Address + ":" + Port);
+        }
+
+        private string InitializePS1CardLink(string ComName)
+        {
+            //Buffer for storing read data from the PS1CLnk
+            byte[] ReadData = null;
 
             //Check if this is PS1CLnk
             SendDataToPort((byte)PS1CLnkCommands.GETID, 100);
@@ -39,7 +68,7 @@ namespace PS1CardLinkCommunication
 
             if (ReadData[0] != 'P' || ReadData[1] != 'S' || ReadData[2] != '1' || ReadData[3] != 'C' || ReadData[4] != 'L' || ReadData[5] != 'N' || ReadData[6] != 'K')
             {
-                return "PS1CardLink was not detected on '" + ComPortName + "' port.";
+                return "PS1CardLink was not detected on '" + ComName + "' port.";
             }
 
             //Get the software version
@@ -55,7 +84,15 @@ namespace PS1CardLinkCommunication
         //Cleanly stop working with PS1CLnk
         public void StopPS1CardLink()
         {
-            if (OpenedPort.IsOpen == true) OpenedPort.Close();
+            if (OpenedPort != null)
+            {
+                if (OpenedPort.IsOpen == true) OpenedPort.Close();
+            }
+            else if (TcpStream != null)
+            {
+                TcpStream.Close();
+                OpenTcpClient.Close();
+            }
         }
 
         //Get the software version of PS1CLnk
@@ -68,13 +105,30 @@ namespace PS1CardLinkCommunication
         private void SendDataToPort(byte Command, int Delay)
         {
             //Clear everything in the input buffer
-            OpenedPort.DiscardInBuffer();
+            FlushPort();
 
             //Send Command Byte
-            OpenedPort.Write(new byte[] { Command }, 0, 1);
+            SendDataToPort(new byte[] { Command }, 0, 1);
 
             //Wait for a required timeframe (for the PS1CLnk response)
             if (Delay > 0) Thread.Sleep(Delay);
+        }
+
+        // Send data to the communication port
+        private void SendDataToPort(byte[] Data, int offset, int count)
+        {
+            if (OpenedPort != null)
+            {
+                OpenedPort.Write(Data, offset, count);
+            }
+            else if (TcpStream != null)
+            {
+                TcpStream.Write(Data, offset, count);
+            }
+            else
+            {
+                // Not initialized properly
+            }
         }
 
         //Catch the response from a PS1CLnk
@@ -84,9 +138,55 @@ namespace PS1CardLinkCommunication
             byte[] InputStream = new byte[256];
 
             //Read data from PS1CLnk
-            if (OpenedPort.BytesToRead != 0) OpenedPort.Read(InputStream, 0, 256);
+            if (OpenedPort != null)
+            {
+                if (OpenedPort.BytesToRead != 0) OpenedPort.Read(InputStream, 0, 256);                
+            }
+            else if (OpenTcpClient != null)
+            {
+                if (OpenTcpClient.Available != 0)
+                {
+                    TcpStream.Read(InputStream, 0, 256);
+                }
+            }
+            else
+            {
+                // Not initialized properly
+            }
 
             return InputStream;
+        }
+
+        private int GetAvailableBytesFromPort()
+        {
+            if (OpenedPort != null)
+            {
+                return OpenedPort.BytesToRead;                
+            }
+            else if (OpenTcpClient != null)
+            {
+                return OpenTcpClient.Available;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private void FlushPort()
+        {
+            if (OpenedPort != null)
+            {
+                OpenedPort.DiscardInBuffer();
+            }
+            else if (OpenTcpClient != null)
+            {
+                ReadDataFromPort();
+            }
+            else
+            {
+                // Not initialized properly
+            }
         }
 
         //Read a specified frame of a Memory Card
@@ -110,7 +210,7 @@ namespace PS1CardLinkCommunication
             SendDataToPort(FrameLsb, 0);
 
             //Wait for the buffer to fill
-            while (OpenedPort.BytesToRead < 130 && DelayCounter < 18)
+            while (GetAvailableBytesFromPort() < 130 && DelayCounter < 18)
             {
                 Thread.Sleep(5);
                 DelayCounter++;
@@ -152,17 +252,17 @@ namespace PS1CardLinkCommunication
                 XorData ^= FrameData[i];
             }
             
-            OpenedPort.DiscardInBuffer();
+            FlushPort();
 
             //Write a frame to the Memory Card
             SendDataToPort((byte)PS1CLnkCommands.MCW, 0);
             SendDataToPort(FrameMsb, 0);
             SendDataToPort(FrameLsb, 0);
-            OpenedPort.Write(FrameData, 0, 128);
+            SendDataToPort(FrameData, 0, 128);
             SendDataToPort(XorData, 0);                      //XOR Checksum
 
             //Wait for the buffer to fill
-            while (OpenedPort.BytesToRead < 1 && DelayCounter < 18)
+            while (GetAvailableBytesFromPort() < 1 && DelayCounter < 18)
             {
                 Thread.Sleep(5);
                 DelayCounter++;
