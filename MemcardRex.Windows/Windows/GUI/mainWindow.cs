93e4a8b37b5558e4545020383686946cfee869e3
kevh182 <154713523+kevh182@users.noreply.github.com>
2025-12-23 22:27:57 -0500

    Update mainWindow.cs
    Add .sav to "All supported" and "standard Memory Card".  Remove from a "new memory card format", as this would mess up header information.  MiSTer FPGA save files are just raw memory card images and additional header information is not needed.

--- MemcardRex.Windows/Windows/GUI/mainWindow.cs
+++ MemcardRex.Windows/Windows/GUI/mainWindow.cs
@@ -91,8 +91,8 @@ namespace MemcardRex
         string tempBufferName = null;
 
         //Supported Memory Card extensions
-        private const string mcSupportedExtensions = "All supported|*.bin;*.ddf;*.gme;*.mc;*.mcd;*.mci;*.mcr;*.mem;*.ps;*.psm;*.srm;*.vgs;*.vm1;*.vmp;*.vmc;*.sav";
-        private const string mcExtensions = "Standard Memory Card|*.mcr;*.bin;*.ddf;*.mc;*.mcd;*.mci;*.ps;*.psm;*.srm;*.vm1;*.vmc;|PSP/Vita Memory Card|*.VMP|PS Vita \"MCX\" PocketStation Memory Card|*.BIN|DexDrive Memory Card|*.gme|MiSTer FPGA (PSX Core) Memory Card|*.sav|VGS Memory Card|*.mem;*.vgs";
+        private const string mcSupportedExtensions = "All supported|*.bin;*.ddf;*.gme;*.mc;*.mcd;*.mci;*.mcr;*.mem;*.ps;*.psm;*.srm;*.vgs;*.vm1;*.vmp;*.vmc;*.sav;";
+        private const string mcExtensions = "Standard Memory Card|*.mcr;*.bin;*.ddf;*.mc;*.mcd;*.mci;*.ps;*.psm;*.srm;*.vm1;*.vmc;*.sav|PSP/Vita Memory Card|*.VMP|PS Vita \"MCX\" PocketStation Memory Card|*.BIN|DexDrive Memory Card|*.gme|VGS Memory Card|*.mem;*.vgs";
 
         //Supported single save extensions
         private const string ssSupportedExtensions = "All supported|*.mcs;*.ps1;*.PSV;*.mcb;*.mcx;*.pda;*.psx;B???????????*";
